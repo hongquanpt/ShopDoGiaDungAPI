74a48c4e7871ae43ed35b07d505445d6c7c333ee
nguyenhongquan122002@gmail.com <nguyenhongquan122002@gmail.com>
2024-11-05 08:59:52 +0700

    a

--- ShopDoGiaDungAPI/DTO/MyOrder.cs
+++ ShopDoGiaDungAPI/DTO/MyOrder.cs
@@ -4,7 +4,7 @@
     {
         public int MaDonHang { get; set; }
         public long? TongTien { get; set; }
-        public string? NguoiNhan { get; set; }11
+        public string? NguoiNhan { get; set; }
         public string? DiaChi { get; set; }
         public DateOnly? NgayMua { get; set; }
         public int? TinhTrang { get; set; }
